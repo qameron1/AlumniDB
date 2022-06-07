dc27f983d2ef35d3278db6ddf994f24160dc6f6a
adamh48 <ahawki56@students.kennesaw.edu>
2022-06-07 15:03:23 -0400

    Update address_table.sql
    added street address to address table

--- address_table.sql
+++ address_table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE address_table(
 addressID int,
 alumniID int,
+address char(55),
 city char(30),
 state char(2),
 zip char(5),
-address_type char(40),
+address_type char(30),
 activeYN char(1),
 primaryYN char(1),
 PRIMARY KEY (addressID),
