caba010442a07c54d841c22bd674ed16588b455d
Wsmith61 <69807232+Wsmith61@users.noreply.github.com>
2022-06-09 12:52:58 -0400

    Update skillset_table.sql

--- skillset_table.sql
+++ skillset_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE skillset(
-skillsetID int,
-alumniID int,
+skillsetID int NOT NULL,
+alumniID int NOT NULL,
 skill char(30) NOT NULL,
-proficiency char(30),
+proficiency char(30) NOT NULL,
 description char(55),
 PRIMARY KEY (skillsetID),
 FOREIGN KEY(alumniID) REFERENCES alumni_table(alumniID)
