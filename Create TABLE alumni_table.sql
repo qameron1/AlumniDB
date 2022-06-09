1e0084bf9b8ecc3f10fc81f35c775838da9e52ee
Wsmith61 <69807232+Wsmith61@users.noreply.github.com>
2022-06-09 12:51:22 -0400

    Update Create TABLE alumni_table.sql

--- Create TABLE alumni_table.sql
+++ Create TABLE alumni_table.sql
@@ -1,5 +1,5 @@
 Create TABLE alumni_table(
-    alumniID int NOT NULL,
+    alumniID int(4) NOT NULL,
     fName char(10) NOT NULL,
     lName char(10) NOT NULL,
     email nchar(20) NOT NULL,
@@ -7,19 +7,19 @@ Create TABLE alumni_table(
     DOB date,
     Gender char(1) NOT NULL,
     Ethnicity tinyint(1) NOT NULL,
-    website char,
-    linkedin_link char,
-    twitter_link char,
-    facebook_link char,
-    instagram_link char,
+    website char(50),
+    linkedin_link char(50),
+    twitter_link char(50),
+    facebook_link char(50),
+    instagram_link char(50)
     guestSpeakerYN char(1)NOT NULL,
     newsLetterYN char(1)NOT NULL,
-    imageThumb char,
-    imageNormal char,
-    alumni_desc char,
+    imageThumb char(50),
+    imageNormal char(50),
+    alumni_desc char(100),
     deceasedYN char(1)NOT NULL,
     deceasedDT date,
-    deceasedNotes char,
+    deceasedNotes char(100),
 
     PRIMARY KEY (alumniID)
 );
