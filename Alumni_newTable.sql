24b67b6aa58cc55074f88a8c35304ee0fbc3788a
Wsmith61 <wsmith61@students.kennesaw.edu>
2022-06-14 14:20:46 -0400

    Connection string

--- Alumni_newTable.sql
+++ Alumni_newTable.sql
@@ -1,9 +1,9 @@
-CREATE TABLE alumi_News(
+CREATE TABLE alumni_News(
 newsID int UNIQUE NOT NULL,
 alumniID int NOT NULL,
 rptDate date NOT NULL,
 headline varchar(45) NOT NULL,
-description varchar(55) NOT NULL, 
+artDesc varchar(55) NOT NULL, 
 link varchar(45) NOT NULL, 
 fileloc varchar(45) NOT NULL,
 PRIMARY KEY (newsID),
--- employment_table.sql
+++ employment_table.sql
@@ -2,6 +2,7 @@ Create TABLE employment_table(
     employmentID int UNIQUE NOT NULL,
     alumniID int NOT NULL,
     company varchar(50) NOT NULL,
+    streetAdd varchar(50),
     city varchar(30),
     stateAdd varchar(2),
     zip int(5),
