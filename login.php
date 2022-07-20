0813e6c9b99b2b4d1cd6b8d28e153e37a1d10af0
qameron1 <89027940+qameron1@users.noreply.github.com>
2022-07-20 19:26:24 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Kennesaw State Alumni Page</title>
+<!-- TemplateEndEditable -->
+<link href="alumni webpage.css"	rel= stylesheet type="text/css">
+<!-- TemplateBeginEditable name="head" -->
+<script src="js/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#menu li').hover(
+        function () {
+            //show submenu
+            $('ul', this).show();
+        }, function () {
+            //hide submenu
+            $('ul', this).hide();
+        });
+    });
+</script>
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<div class="container">
+  <header>
+    <div class="primary_header">
+      <h1 class="title"> Kennesaw State University Alumni</h1>
+    </div>
+<!-- TemplateBeginEditable name="Nav Bar" -->
+	 <nav class="secondary_header">
+     
+  <footer class="secondary_header footer">
+	  
+    <div class="copyright">
+<div class="row">
+<div class="col  center" style="margin-top: 10%">
+<h1>KSU Alumni Login</h1>
+<form method='post' action='Unnamed Site 2/enter.php'>
+<div><p>Enter Username</p>
+<p ><input type='text' name='follow' style="margin-left: auto; Margin-right:auto"></p>
+	<div><p>Enter Password</p>
+<p ><input type='password' name='follow' style="margin-left: auto; Margin-right:auto">
+</p>
+<button name="submit" value="submit" type="submit">Continue</button> <p>Not yet a member? 
+<a href="Account_Registration_CT7PJE8Z6L.php">Register</a> &nbsp;</p>
+
+</form>
+</div>&copy;2022 - <strong>Capstone summer 2022</strong></div>
+  </footer>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Kennesaw State Alumni Page</title>
+<!-- TemplateEndEditable -->
+<link href="../alumni webpage.css"	rel= stylesheet type="text/css">
+<!-- TemplateBeginEditable name="head" -->
+<script src="../js/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#menu li').hover(
+        function () {
+            //show submenu
+            $('ul', this).show();
+        }, function () {
+            //hide submenu
+            $('ul', this).hide();
+        });
+    });
+</script>
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<div class="container">
+  <header>
+    <div class="primary_header">
+      <h1 class="title"> Kennesaw State University Alumni</h1>
+    </div>
+<!-- TemplateBeginEditable name="Nav Bar" -->
+	 <nav class="secondary_header">
+     
+  <footer class="secondary_header footer">
+	  
+    <div class="copyright"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                   		  <tbody><tr>
+                         			<td colspan="2" align="left">	
+	<table summary="Error Messages" width="100%" border="0" cellspacing="0" cellpadding="2">
+	</table>
+ 
+
+</td>
+											  
+				</tr>
+
+
+                                   		
+                                             <tr>
+					<h1><strong>User Registry</strong></h1>
+						
+							    
+				</tr>
+											  
+				<tr>
+					<td width="25%"></td>
+					<br></br>
+	
+				</tr>
+				<tr>
+					<td class="accounttext">E-mail Address:<font color="red">*</font></td>
+					<td class="carttext"><input name="LEMAIL" size="24" maxlength="50" value=""></td>
+				</tr>
+				<tr>
+					<td class="accounttext">Confirm e-mail address:<font color="red">*</font></td>
+					<td class="carttext"><input name="LVEMAIL" size="24" maxlength="50" value="">
+					</td>
+				</tr>
+				<tr>
+					<td width="25%"></td>
+					<br></br>
+				</tr>
+				<tr>
+					<td class="accounttext">Choose a Password:<font color="red">*&nbsp;</font></td>
+					<td class="carttext"><input type="password" name="APASSWORD" size="20" maxlength="20" value="">
+					</td>
+				</tr>
+				<tr>
+					<td class="accounttext">Confirm Password:<font color="red">*&nbsp;</font></td>
+					<td class="carttext"><input type="password" name="AVPASSWORD" size="20" maxlength="20" value=""></td>
+				</tr>
+                                    	 <tr>
+                         			<td colspan="2"><img height="10" alt="" src="/images/jb/spacer.gif" width="12" border="0"></td>
+				</tr>
+
+<tr><td colspan="2">
+           <table width="100%" cellspacing="0" cellpadding="0" border="0">
+             <tbody><tr><td width="100%"><img src="/images/jb/spacer.gif" width="1" height="5" alt="" border="0"></td></tr>
+<button name="submit" value="submit" type="submit">Creat Account</button>
+				</tbody></table>&copy;2022 - <strong>Capstone summer 2022</strong></div>
+  </footer>
+</body>
+</html>
